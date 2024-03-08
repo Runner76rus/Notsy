ef2cf0fe70de44009637b596eb06b39f56f4e32c
Anton Yazgevich <yazgevich@mail.ru>
2024-03-08 15:11:02 +0300

    add a User model

---
+++ src/main/java/ru/natsy/model/User.java
@@ -0,0 +1,49 @@
+package ru.natsy.model;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
