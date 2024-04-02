1bb2f72a69e334034eee6f227d486f8db20591a1
Anton Yazgevich <yazgevich@mail.ru>
2024-04-02 14:01:43 +0300

    add username and password fields

--- src/main/java/ru/natsy/model/User.java
+++ src/main/java/ru/natsy/model/User.java
@@ -2,10 +2,29 @@ package ru.natsy.model;
 
 public class User {
     private long id;
+    private String username;
+
+    private String password;
     private String firstName;
     private String secondName;
     private String email;
     private String phoneNumber;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getPhoneNumber() {
         return phoneNumber;
